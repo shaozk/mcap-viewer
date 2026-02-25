c8c04a2c6f6c7011ea4b7187da209e3e1f15a9b6
shaozikang <shaozikang@minieye@.cc>
2026-02-25 17:56:48 +0800

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# mcap-viewer
+
+macp-viewer 是一个基于 [mcap](https://github.com/foxglove/mcap) 的文件查看器。
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mcap-viewer!")
+
+
+if __name__ == "__main__":
+    main()

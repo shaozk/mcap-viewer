bfc1e625bc5be41f4d2fc53300c2b0bb0ca046ac
shaozikang <shaozikang@minieye@.cc>
2026-02-28 23:00:38 +0800

    test mcap lib

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
+from mcap.reader import make_reader, McapReader
+
+from src.utils.json_util import dumps_json
+from pathlib import Path
+
+
+def process_attachments(reader: McapReader):
+    for attachment in reader.iter_attachments():
+        print(f"附件名称: {attachment.name}")
+        print(f"媒体类型: {attachment.media_type}")
+        print(f"数据大小: {len(attachment.data)} 字节")
+        file_path = Path(attachment.name)
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.touch()
+        with open(f"{attachment.name}", "wb") as output_file:
+            output_file.write(attachment.data)
+
+
+def process_metadata(reader: McapReader):
+    for metadata in reader.iter_metadata():
+        print(f"数据名称: {metadata.name}")
+        print(f"数据大小: {len(metadata.metadata)} 字节")
+        print(f"数据: {dumps_json(metadata.metadata)}")
+
+
+def process_schemas(reader: McapReader):
+    for schema, channel, message in reader.iter_messages():
+        print(schema.name, schema.encoding, schema.id)
+        print(channel.topic, channel.schema_id, channel.message_encoding, channel.id)
+        print(message.log_time, message.publish_time, message.channel_id, message.sequence)
+
+
 def main():
     print("Hello from mcap-viewer!")
+    mcap_path = "/home/mini/data/mcap/019afdfe-907a-73da-71c4-9d50e21091ad.mcap"
+    with open(mcap_path, "rb") as f:
+        reader = make_reader(f)
+        summary = reader.get_summary()
+        print(summary)
+
+        header = reader.get_header()
+        print(header)
+
+        # process_attachments(reader)
+        # process_metadata(reader)
+        process_schemas(reader)
 
 
 if __name__ == "__main__":
---
+++ src/utils/json_util.py
@@ -0,0 +1,37 @@
+import os
+import json
+
+
+def json_to_string(json_data):
+    return json.dumps(json_data, ensure_ascii=False, indent=4)
+
+
+def load_json(fp, nullable=True, default=None):
+    if fp is None and nullable:
+        return default
+    elif not os.path.exists(fp):
+        raise FileNotFoundError(fp)
+    with open(fp, 'r') as fr:
+        text = fr.read().strip()
+        if not text:
+            return default
+        try:
+            data = json.loads(text)
+            return data
+        except Exception as e:
+            print(fp, e)
+            return default
+
+
+def save_json(data, fname):
+    with open(fname, 'w') as f:
+        if isinstance(data, str):
+            text = data
+        else:
+            text = json.dumps(data, indent=4, ensure_ascii=False)
+        f.write(text)
+    return fname
+
+
+def dumps_json(data):
+    return json.dumps(data, indent=4, ensure_ascii=False)
